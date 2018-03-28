1392b9a30858a4d1d9ec72c76dcc6e449d039eb8
DrMegavolt <sergeykuc@gmail.com>
2018-03-28 23:31:50 +0300

    list directory

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-module.exports = {
-  serialize (rootPath) {
-
-  },
+require('util.promisify/shim')()
+const { promisify } = require('util')
+const fs = require('fs')
+const writeFile = promisify(fs.writeFile)
+const rmFile = promisify(fs.unlink)
+const readdir = promisify(fs.readdir)
+const readFile = promisify(fs.readFile)
+const path = require('path')
+class DirectoryAsObject {
+  constructor ({rootPath, ignorePatterns = []}) {
+    this.rootPath = rootPath
+    this.ignorePatterns = ignorePatterns
+  }
+  serialize () {
+    readdir(this.rootPath).then(filenames => {
+    // Ignore files/sub dirs matching regex patterns
+      return filenames
+        .filter(name => !this.ignorePatterns.some(pattern => pattern.test(name)))
+        .map((filename) => {
+          return readFile(path.join(this.rootPath, filename), 'utf-8')
+            .then(content => { return { filename, content } })
+        })
+    })
+  }
   deserialize (data) {
 
   }
 }
+
+module.exports = DirectoryAsObject
