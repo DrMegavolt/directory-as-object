8c97a53e38d01b1d07127fbd6eeef2430631ec7e
DrMegavolt <sergeykuc@gmail.com>
2018-03-29 00:28:28 +0300

    read directory deeply

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const writeFile = promisify(fs.writeFile)
 const rmFile = promisify(fs.unlink)
 const readdir = promisify(fs.readdir)
 const readFile = promisify(fs.readFile)
+const fileStat = promisify(fs.lstat)
 const path = require('path')
 class DirectoryAsObject {
   constructor ({rootPath, ignorePatterns = []}) {
@@ -12,18 +13,43 @@ class DirectoryAsObject {
     this.ignorePatterns = ignorePatterns
   }
   serialize () {
-    readdir(this.rootPath).then(filenames => {
-    // Ignore files/sub dirs matching regex patterns
-      return filenames
-        .filter(name => !this.ignorePatterns.some(pattern => pattern.test(name)))
-        .map((filename) => {
-          return readFile(path.join(this.rootPath, filename), 'utf-8')
-            .then(content => { return { filename, content } })
-        })
-    })
+    return readdir(this.rootPath)
+      .then(filenames => {
+        // Ignore files/sub dirs matching regex patterns
+        return filenames
+          .filter(name => !this.ignorePatterns.some(pattern => pattern.test(name)))
+          .map((filename) => {
+            return this.readFileOrFolder(filename)
+          })
+      })
+      .then(readFileOps => { return Promise.all(readFileOps) })
+      .then(fileInfos => {
+        console.log('FFF', fileInfos)
+
+        return fileInfos.reduce((result, curr) => {
+          result[curr.filename] = curr.content
+          return result
+        }, {})
+      })
   }
   deserialize (data) {
 
+  }
+  readFileOrFolder (filename) {
+    let p = path.join(this.rootPath, filename)
+    return fileStat(p)
+      .then(stat => {
+        if (stat.isFile()) {
+          return readFile(p, 'utf-8')
+            .then(content => {
+              return { filename: filename, content }
+            })
+        } else {
+          let recursiveDirAsObject = new DirectoryAsObject({rootPath: p, ignorePatterns: this.ignorePatterns})
+          return recursiveDirAsObject.serialize()
+            .then(content => { return { filename: filename, content } })
+        }
+      })
   }
 }
 
